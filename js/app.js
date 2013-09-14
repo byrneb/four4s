c3a2646f338fec146f7aa9ad74b7e9ba3d25564a
byrneb <brian.byrne25@student.dit.ie>
2013-09-14 20:10:16 +0100

    Stage two

--- js/app.js
+++ js/app.js
@@ -1,16 +1,142 @@
+ var app = app || {};
+  app.SolutionModel = Backbone.Model.extend({
+	
+  });
+  
+  app.TargetView  = Backbone.View.extend({
+ 
+	tagName: 'div',
+	id: 'target',
+	initialize: function(){
+		_.bindAll(this, 'render');
+		this.template = _.template($('#target-template').html());
+		this.model = new Backbone.Model();
+		this.model.set('target',1);
+	},
+	
+	render: function (){
+		var renderedContent = this.template(this.model.toJSON());
+		$(this.el).html(renderedContent);
+		return this;
+	}
+});
+
+ app.SolutionView  = Backbone.View.extend({
+ 
+	tagName: 'div',
+	id: 'sol-container',
+	initialize: function(){
+		_.bindAll(this, 'render');
+		this.model.on('change:solution', this.render);
+		this.template = _.template($('#sol-template').html());
+	},
+	
+	render: function (){
+		sum = 0;
+		try{
+			sum = makeReadable(this.model.get('solution'));
+			sum = eval(sum);
+		}catch(e)
+		{sum = 0;}
+		this.model.set('total', ('=  '+sum)); 
+		var renderedContent = this.template(this.model.toJSON());
+		$(this.el).html(renderedContent);
+		return this;
+	}
+});
+ 
+  app.SymbolsView  = Backbone.View.extend({
+ 
+	tagName: 'div',
+	className: 'symbols unselectable',
+	initialize: function(){
+		_.bindAll(this, 'render');
+		this.template = _.template($('#symbols-template').html());
+		
+		this.fourView = new app.SymbolView({model: new Backbone.Model({symbol: "4"}), 
+											solutionModel:this.model});
+		this.plusView = new app.SymbolView({model: new Backbone.Model({symbol: "+"}), 
+											solutionModel:this.model});
+		this.minusView = new app.SymbolView({model: new Backbone.Model({symbol: "-"}), 
+											solutionModel:this.model});
+		this.decimalView = new app.SymbolView({model: new Backbone.Model({symbol: "·"}), 
+											solutionModel:this.model});
+		this.multiplyView = new app.SymbolView({model: new Backbone.Model({symbol: "×"}), 
+											solutionModel:this.model});
+		this.divideView = new app.SymbolView({model: new Backbone.Model({symbol: "÷"}), 
+											solutionModel:this.model});
+		this.bracketlView = new app.SymbolView({model: new Backbone.Model({symbol: "("}),
+												solutionModel:this.model, id:"bracket"});
+		this.bracketrView = new app.SymbolView({model: new Backbone.Model({symbol: ")"}),
+												solutionModel:this.model, id:"bracket"});
+	},
+	
+	render: function (){
+		var renderedContent = this.template();
+		$(this.el).html(renderedContent);
+		
+		this.$el.append(this.fourView.render().el,this.plusView.render().el,this.minusView.render().el,
+						this.decimalView.render().el,this.multiplyView.render().el,this.divideView.render().el,
+						this.bracketlView.render().el,this.bracketrView.render().el);
+		return this;
+	}
+});
+
+ app.SymbolView  = Backbone.View.extend({
+ 
+	tagName: 'div',
+	className: 'key',
+	
+	events: {
+		"click": "keyClicked"
+    },
+	
+	initialize: function(){
+		_.bindAll(this, 'render', 'keyClicked');
+		this.solutionModel = this.options.solutionModel;
+		this.template = _.template($('#key-template').html());
+	},
+	
+	render: function (){
+		var renderedContent = this.template(this.model.toJSON());
+		$(this.el).html(renderedContent);
+		return this;
+	},
+	
+	keyClicked: function(){
+		updatedSolution = this.solutionModel.get('solution') + this.model.get('symbol');
+		this.solutionModel.set('solution', updatedSolution);
+	}
+});
+
  Router = Backbone.Router.extend({
 	routes: {
         "" : "home"
 		},
 	initialize: function(){
-		
+		this.solutionModel = new app.SolutionModel({solution:"", total:""});
+		this.targetView = new app.TargetView();
+		this.solutionView = new app.SolutionView({model:this.solutionModel});
+		this.symbolsView = new app.SymbolsView({model:this.solutionModel});
 	},
 	home:function() {
-		var content = $('#content');
+		var content = $('#four4sApp');
 		content.empty();
+		content.append(this.targetView.render().el);
+		content.append(this.solutionView.render().el);
+		content.append(this.symbolsView.render().el);
 	}
 });
+
+makeReadable = function(original){
+	if(original === '')
+		return 0;
+	result = original.replace(/·/g,'.');
+	result = result.replace(/÷/g,'/');
+	result = result.replace(/×/g,'*');
+	return result;
+};
  $(function() {
-    app = new Router();
+    app.router = new Router();
     Backbone.history.start();
 });
