4fcf2a914534b88679687e40cee8fea50bf25d76
byrneb <brian.byrne25@student.dit.ie>
2013-12-01 16:23:56 +0000

    Update header size calculation

--- js/app.js
+++ js/app.js
@@ -1,88 +1,88 @@
- var app = app || {};
- 
-   app.Puzzle = Backbone.Model.extend({
+var app = app || {};
+
+app.Puzzle = Backbone.Model.extend({
    
-		update: function(input){			
-			this.updateTotal();	
-			this.checkTargetReached();
-			
-			if(this.foursCountReached())
-				$( ".icon-four-key" ).addClass( "gray" );			
-			else
-				$( ".icon-four-key" ).removeClass( "gray" );
-		},
-		
-		addToSolution: function(input){			
-			var updatedSolution = this.get('solution') + input;			
-			this.set('solution', updatedSolution);
-			
-			if(input == 4)
-				this.incrementFoursCount();
-			this.update();
-		},
-		
-		removeFromSolution: function(){
-			var solution = this.get('solution');
-			this.set('solution', solution.slice(0,-1));
-			
-			var removedChar = solution.charAt(solution.length-1);
-			if(removedChar === '4')
-				this.decrementFoursCount();
-			this.update();
-		},
-		
-		incrementFoursCount: function(){			
-			var foursCount = this.get('foursCount');
-			this.set('foursCount', foursCount+1);
-		},
-		
-		decrementFoursCount: function(){			
-			var foursCount = this.get('foursCount');
-			this.set('foursCount', foursCount-1);
-		},
-		
-		foursCountReached: function(){
-			return this.get('foursCount') === 4;
-		},
-		
-		updateTotal: function(){			
-			var sum = 0;
-			try{
-				sum = makeReadable(this.get('solution'));
-				sum = eval(sum);
-				sum = Number(sum.toFixed(2));
-			}
-			catch(e){
-				sum = 0;
-			}
-			this.set('total', (sum)); 
-		},
-		
-		checkTargetReached: function(){
-			var target = this.get('target');
-			if((this.get('total') === target) && (this.foursCountReached())){
-				modal = new app.ModalView({model: this});
-				modal.show();
-			}
-		},
-		
-		nextTarget: function(){
-			var target = this.get('target');
-			this.set('target', target+1);
-			this.clearSolution();
-			localStorage.setItem( 'target', JSON.stringify(target+1) );
-		},
-		
-		clearSolution: function(){
-			this.set('solution', '');
-			this.set('total', 0);
-			this.set('foursCount', 0);
+	update: function(input) {
+		this.updateTotal();
+		this.checkTargetReached();
+
+		if(this.foursCountReached())
+			$( ".icon-four-key" ).addClass( "gray" );
+		else
 			$( ".icon-four-key" ).removeClass( "gray" );
-		}
+	},
+
+	addToSolution: function(input){
+		var updatedSolution = this.get('solution') + input;
+		this.set('solution', updatedSolution);
+
+		if(input == 4)
+			this.incrementFoursCount();
+		this.update();
+	},
 		
-  });
+	removeFromSolution: function(){
+		var solution = this.get('solution');
+		this.set('solution', solution.slice(0,-1));
+		
+		var removedChar = solution.charAt(solution.length-1);
+		if(removedChar === '4')
+			this.decrementFoursCount();
+		this.update();
+	},
+		
+	incrementFoursCount: function(){
+		var foursCount = this.get('foursCount');
+		this.set('foursCount', foursCount+1);
+	},
+		
+	decrementFoursCount: function(){
+		var foursCount = this.get('foursCount');
+		this.set('foursCount', foursCount-1);
+	},
+		
+	foursCountReached: function(){
+		return this.get('foursCount') === 4;
+	},
+	
+	updateTotal: function(){
+		var sum = 0;
+		try{
+			sum = makeReadable(this.get('solution'));
+			sum = eval(sum);
+			sum = Number(sum.toFixed(2));
+		}
+		catch(e){
+			sum = 0;
+		}
+		this.set('total', (sum));
+	},
+	
+	checkTargetReached: function(){
+		var target = this.get('target');
+		if((this.get('total') === target) && (this.foursCountReached())){
+			modal = new app.ModalView({model: this});
+			modal.show();
+		}
+	},
+	
+	nextTarget: function(){
+		var target = this.get('target');
+		this.set('target', target+1);
+		this.clearSolution();
+		localStorage.setItem( 'target', JSON.stringify(target+1) );
+	},
+	
+	clearSolution: function(){
+		this.set('solution', '');
+		this.set('total', 0);
+		this.set('foursCount', 0);
+		$( ".icon-four-key" ).removeClass( "gray" );
+	}
+		
+});
   
-  app.HeaderView  = Backbone.View.extend({
+app.HeaderView  = Backbone.View.extend({
  
 	tagName: 'div',
 	id: 'header',
@@ -101,24 +101,7 @@
 		var renderedContent = this.template(this.model.toJSON());
 		$(this.el).html(renderedContent);
 		
-		var bodyHeight = $('body').height(); 
-		var bodyWidth = $('body').width();		  
-		 
-		var homeOptionWidth = bodyHeight*.14;
-		var hintOptionWidth = bodyHeight*.13;
-		this.$( "#home" ).css( "width", homeOptionWidth); 
-		this.$( "#hint" ).css( "width", hintOptionWidth); 
-		  
-		var headerTotalWidth = bodyWidth - (2*bodyHeight*.14); 
-		this.$( "#target" ).css( "width", headerTotalWidth);
-		
-		var marginTop = bodyHeight*.31*.145; 
-		this.$( ".fa, #target").css( "margin-top", marginTop);
-		  
-		  
-		var headerFontSize = .4*bodyHeight*.13;
-		this.$( "#home, #hint, #target" ).css( "fontSize", headerFontSize ); 
-		this.delegateEvents(); 
+		this.delegateEvents();
 		return this;
 	},
 	
@@ -143,15 +126,15 @@
 		var renderedContent = this.template(this.model.toJSON());
 		$(this.el).html(renderedContent);
 		
-		var bodyHeight = $('body').height();	  
-		var marginTop = bodyHeight*.1; 	  
-		var marginBottom = bodyHeight*.015; 		
-		
-		this.$el.css( "margin-top", marginTop ); 
-		this.$( "#solution" ).css( "margin-bottom", .025*bodyHeight ); 
-		this.$( "#solution" ).css( "font-size", .055*bodyHeight ); 
-		this.$( "#total" ).css( "font-size", .13*bodyHeight ); 
-		this.delegateEvents(); 
+		var bodyHeight = $('body').height();
+		var marginTop = bodyHeight*0.1;
+		var marginBottom = bodyHeight*0.015;
+
+		this.$el.css( "margin-top", marginTop );
+		this.$( "#solution" ).css( "margin-bottom", 0.025*bodyHeight );
+		this.$( "#solution" ).css( "font-size", 0.055*bodyHeight );
+		this.$( "#total" ).css( "font-size", 0.13*bodyHeight );
+		this.delegateEvents();
 		return this;
 	}
 });
@@ -160,23 +143,22 @@
  
 	tagName: 'div',
 	className: 'symbols ',
-	initialize: function(){			
-		this.symbolViewArray = new Array();
+	initialize: function(){
+		this.symbolViewArray = [];
 		var symbolsArray = ['4', '+', '-', '·', '×', '÷', '(', ')', '«'];
 		var classArray = ['four-key', 'plus-key', 'minus-key', 'dot-key', 'mutliply-key', 'divide-key', 'left-bracket-key', 'right-bracket-key', 'back-key'];
 		_.bindAll(this, 'render');
-		this.template = _.template($('#symbols-template').html());	
-		for (var i = 0; i < symbolsArray.length; i++) 
+		this.template = _.template($('#symbols-template').html());
+		for (var i = 0; i < symbolsArray.length; i++)
 			this.symbolViewArray[i] = new app.SymbolView( { className : 'key icon-'+classArray[i], symbol : symbolsArray[i], solutionModel:this.model});
-		
 	},
 	
 	render: function (){
 		$(this.el).html(this.template);
 		
-		for (var i = 0; i < this.symbolViewArray.length; i++) 
+		for (var i = 0; i < this.symbolViewArray.length; i++)
 			this.$el.append(this.symbolViewArray[i].render().el);
-		this.delegateEvents(); 	
+		this.delegateEvents();
 		return this;
 	}
 });
@@ -200,19 +182,19 @@
 	render: function (){
 		$(this.el).html(this.template);
 		 
-		var bodyHeight = $('body').height(); 
-		  
-		var diameter = .1 * bodyHeight;
-		var marginTop = .04 * bodyHeight;
-		this.$el.css( "fontSize", diameter ); 
-		this.$el.css( "margin-top", marginTop ); 
-		this.delegateEvents(); 
+		var bodyHeight = $('body').height();
+
+		var diameter = 0.1 * bodyHeight;
+		var marginTop = 0.04 * bodyHeight;
+		this.$el.css( "fontSize", diameter);
+		this.$el.css( "margin-top", marginTop);
+		this.delegateEvents();
 		return this;
 	},
 	
 	keyClicked: function(){
 		if (this.symbol === '«')
-			this.solutionModel.removeFromSolution();	
+			this.solutionModel.removeFromSolution();
 		else if( !((this.symbol === '4') && this.solutionModel.foursCountReached()) )
 			this.solutionModel.addToSolution(this.symbol);
 			
@@ -233,17 +215,17 @@ app.ModalView = Backbone.View.extend({
         render: function() {
 			this.model.set('title', "Success");
             this.$el.html(this.template(this.model.toJSON()));
-			var bodyHeight = $('body').height(); 
+			var bodyHeight = $('body').height();
 		
-			this.$( ".modal-header" ).css( "font-size", .07*bodyHeight); 
-			this.$( ".modal-header" ).css( "margin-top", .04*bodyHeight); 
-			this.$( ".strikethrough" ).css( "margin-top", .085*bodyHeight); 
-			this.$( ".strikethrough" ).css( "font-size", .27*bodyHeight); 
+			this.$( ".modal-header" ).css( "font-size", 0.07*bodyHeight);
+			this.$( ".modal-header" ).css( "margin-top", 0.04*bodyHeight);
+			this.$( ".strikethrough" ).css( "margin-top", 0.085*bodyHeight);
+			this.$( ".strikethrough" ).css( "font-size", 0.27*bodyHeight);
             return this;
         },
 
         show: function() {
-            $(document.body).append(this.render().el);                
+            $(document.body).append(this.render().el);
         },
 
         close: function() {
@@ -257,24 +239,24 @@ app.ModalView = Backbone.View.extend({
   app.HomeView  = Backbone.View.extend({
  
 	tagName: 'div',
-	id: 'home-screen',	
+	id: 'home-screen',
 	events: {
 		"click #exit": "exit",
 		"click #option-play": "play"
     },
-	
-	initialize: function(){		
+
+    initialize: function(){
 		_.bindAll(this, 'render');
-		this.template = _.template($('#home-template').html());			
+		this.template = _.template($('#home-template').html());
 	},
 	
 	render: function (){
 		$(this.el).html(this.template);
-		var height = $('body').height()*.08; 
-		var fontSize = $('body').height()*.033; 
-		this.$( ".leftside-edge" ).css( "width", height); 
-		this.$( ".rightside-edge" ).css( "width", height); 
-		this.$( ".menu-option" ).css( "margin-left", -height/2); 
+		var height = $('body').height()*0.08;
+		var fontSize = $('body').height()*0.033;
+		this.$( ".leftside-edge" ).css( "width", height);
+		this.$( ".rightside-edge" ).css( "width", height);
+		this.$( ".menu-option" ).css( "margin-left", -height/2);
 		this.$( ".menu-option" ).css( "margin-right", -height/2);
 		this.$( ".menu-option" ).css( "font-size", fontSize);
 		
@@ -291,7 +273,7 @@ app.ModalView = Backbone.View.extend({
 });
  Router = Backbone.Router.extend({
 	routes: {
-        "" : "home", 
+        "" : "home",
 		"play" : "play"
 		},
 	initialize: function(){
@@ -305,11 +287,11 @@ app.ModalView = Backbone.View.extend({
 		this.HeaderView = new app.HeaderView({model:this.puzzle});
 		this.solutionView = new app.SolutionView({model:this.puzzle});
 		this.symbolsView = new app.SymbolsView({model:this.puzzle});
-		this.homeView = new app.HomeView();	
-		
+		this.homeView = new app.HomeView();
 	},
-	home:function() {	
-		
+
+	home:function() {
+
 		var content = $('#four4sApp');
 		content.empty();
 		
@@ -340,10 +322,75 @@ makeReadable = function(original){
 	result = result.replace(/×/g,'*');
 	return result;
 };
+
+		function changecss(theClass,element,value) {
+		//Last Updated on July 4, 2011
+		//documentation for this script at
+		//http://www.shawnolson.net/a/503/altering-css-class-attributes-with-javascript.html
+		 var cssRules;
+
+
+		 for (var S = 0; S < document.styleSheets.length; S++){
+
+
+			  try{
+			  	document.styleSheets[S].insertRule(theClass+' { '+element+': '+value+'; }',document.styleSheets[S][cssRules].length);
+
+			  } catch(err){
+			  		try{document.styleSheets[S].addRule(theClass,element+': '+value+';');
+
+					}catch(err){
+
+					 	try{
+						    if (document.styleSheets[S]['rules']) {
+							  cssRules = 'rules';
+							 } else if (document.styleSheets[S]['cssRules']) {
+							  cssRules = 'cssRules';
+							 } else {
+							  //no rules found... browser unknown
+							 }
+
+							  for (var R = 0; R < document.styleSheets[S][cssRules].length; R++) {
+							   if (document.styleSheets[S][cssRules][R].selectorText == theClass) {
+							    if(document.styleSheets[S][cssRules][R].style[element]){
+							    document.styleSheets[S][cssRules][R].style[element] = value;
+								break;
+							    }
+							   }
+							  }
+						  } catch (err){}
+
+
+
+					}
+
+			  }
+
+
+		}
+	}
+
  $(function() {
-	 window.addEventListener('load', function() {
+	window.addEventListener('load', function() {
 		FastClick.attach(document.body);
 	}, false);
+
+		var bodyHeight = $('body').height();
+	var bodyWidth = $('body').width();
+	 
+	var homeOptionWidth = bodyHeight*0.14;
+	var hintOptionWidth = bodyHeight*0.13;
+	changecss('#hint','width',hintOptionWidth+'px');
+	changecss('#home','width',homeOptionWidth+'px');
+
+	var headerTotalWidth = bodyWidth - (2*bodyHeight*0.14);
+	changecss('#target','width',headerTotalWidth+'px');
+	
+	var marginTop = bodyHeight*0.31*0.145;
+	changecss('.fa, #target','margin-top',marginTop+'px');
+
+	var headerFontSize = 0.4*bodyHeight*0.13;
+	changecss('#home, #hint, #target','font-size',headerFontSize+'px');
     app.router = new Router();
     Backbone.history.start();
 });
