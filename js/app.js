7258015caeeeff92236904c3e12e4287efc703e2
byrneb <brian.byrne25@student.dit.ie>
2014-03-10 01:22:38 +0000

    hint beginings

--- js/app.js
+++ js/app.js
@@ -438,7 +438,7 @@ app.HeaderView = Backbone.View.extend({
 
 	hint : function(){
 	   var e = document.getElementById("hint-container");
-       if(e.style.display != 'none')
+       if(e.style.display == 'block')
           e.style.display = 'none';
        else
           e.style.display = 'block';
